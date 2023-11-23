f505cd0472c190958320e58fd3d98c6702dd10e5
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-11-23 20:31:17 -0300

    Video 1.

--- secao_3/variaveis-e-tipos.js
+++ secao_3/variaveis-e-tipos.js
@@ -1,1 +1,8 @@
-let age = 20 //number 
+let age = 20; //number 
+let fullName = 'Lindomar';//string
+let isValid = true; //boolean
+let xablau; // undefined
+let value = null; // null
+
+
+console.log("Seu nome é: " + fullName, " e você tem ", age, " Anos.");
