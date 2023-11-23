366b039720b82010974366ff518495584f67ee2c
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-11-23 20:40:02 -0300

    Fim da Aula e fixar.

---
+++ secao_3/parafixar1.js
@@ -0,0 +1,5 @@
+let myName = 'Jaélson'
+const birthCity = 'Balneário Camboriu'
+const birthYear = 1990
+
+console.log('Seu nome é ', myName, 'Você mora em ', birthCity, ' e nasceu em ', birthYear);
--- secao_3/variaveis-e-tipos.js
+++ secao_3/variaveis-e-tipos.js
@@ -6,3 +6,10 @@ let value = null; // null
 
 
 console.log("Seu nome é: " + fullName, " e você tem ", age, " Anos.");
+
+let patientId = '50';
+let isEnrolled = true;
+let patientEmail = 'ana@email.com';
+
+console.log(typeof patientId); // number
+console.log(typeof patientAge);
