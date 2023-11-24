eae9fbeed00fb355fba09b45266bcd4274e89df2
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-11-24 00:16:17 -0300

    Exercicio 1.

---
+++ secao_3/exercicios/exercicio_1.js
@@ -0,0 +1,22 @@
+//Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. É necessário que seu código tenha duas variáveis, num1 e num2, definidas no começo com os valores que serão operados. Escreva códigos para:
+
+//Adição (num1 + num2)
+//Subtração (num1 - num2)
+//Multiplicação (num1 * num2)
+//Divisão (num1 / num2)
+//Módulo (num1 % num2)
+
+let number1 = 2;
+let number2 = 4;
+
+let adicao = number1 + number2;
+let subtracao = number1 - number2;
+let multiplication = number1 * number2;
+let division = number1 / number2;
+let modulo = number1 % number2;
+
+console.log(`A soma de ${number1} + ${number2} é igual á ${adicao}`);
+console.log(`A subtracão de ${number1} - ${number2} é igual á ${subtracao}`);
+console.log(`A multiplicacão de ${number1} X ${number2} é igual á ${multiplication}`);
+console.log(`A divisão de ${number1} / ${number2} é igual á ${division}`);
+console.log(`O resto da divisão ${number1} % ${number2} é igual á ${modulo}`);
