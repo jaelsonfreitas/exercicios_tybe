41a62b9c4968a1ac63772b09cfe83b14ce5b279a
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-11-25 21:04:38 -0300

    Exercicio 6

---
+++ secao_3/exercicios/exercicio_6.js
@@ -0,0 +1,10 @@
+let num1 = 10;
+let num2 = 7;
+let num3 = 9;
+
+
+if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0) {
+    console.log('Temos um numero par');
+}else{
+    console.log('Não temos um numero par.');
+}
