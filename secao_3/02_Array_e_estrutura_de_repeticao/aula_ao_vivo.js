ef4ad3db52022d8c0d9174825aba0bde81d35879
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-11-28 00:04:10 -0300

    Final exercicio.

--- secao_3/02_Array_e_estrutura_de_repeticao/aula_ao_vivo.js
+++ secao_3/02_Array_e_estrutura_de_repeticao/aula_ao_vivo.js
@@ -1,7 +1,17 @@
 // Jogo do Estudante.
-const jaelGamer = [7, 13, 21, 39, 44, 51];
+const jaelGamer = [3, 25, 23, 15, 21, 12, 13, 11, 9, 4, 1, 6, 24, 18, 5 ];
 // Jogo Sorteado
-const resultLottery = [6, 12, 13, 20, 38, 51];
+let megaSenaNumbers = [];
+for (let index = 0; index < 15; index += 1){
+    const number = Math.floor(Math.random()* 25)+1;
+
+    if(megaSenaNumbers.includes(number)){
+        index -= 1
+    } else {
+        megaSenaNumbers.push(number);
+    }
+}
+
 
 
 // conferindo o Jogo
@@ -10,15 +20,16 @@ let numberOfHits = 0;
 for ( let i = 0; i < jaelGamer.length; i += 1){
     let jaelNumber = jaelGamer[i];
 
-    for (let index = 0; index < resultLottery.length; index+= 1) {
-        let element = resultLottery[index];
+    for (let index = 0; index < megaSenaNumbers.length; index+= 1) {
+        let element = megaSenaNumbers[index];
         
         if (jaelNumber === element) {
             numberOfHits += 1
         } 
     }
 }
-
+console.log(jaelGamer);
+console.log(megaSenaNumbers);
 console.log(`Numero de acertos, ${numberOfHits}`);
 // criando for - sempre iniciamos com o index, e sempre informo de onde ele começa
 
