0253d56fe147376a5dd173596e43f9ccbced1066
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-11-28 21:42:47 -0300

    conteudo.

---
+++ secao_3/03_funcoes/arrowfunctions.js
@@ -0,0 +1,16 @@
+// function sumArray(array) 
+
+// const sumArray = (array) => {
+//     let sum =0;
+//     for (let index = 0; index < array.length; index += 1) {
+//         sum = sum + array[index];
+//     }
+//     return sum;
+//     };
+
+//     console.log(sumArray([10, 20, 30, 40,50]));
+
+const countWods = (phrare) => phrare.split(' ').length
+
+
+console.log(countWods('Fala tribo, beleza?'));
