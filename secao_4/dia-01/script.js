23221032d48e1432b3d7034d084775252956bbb9
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-12-07 23:07:40 -0300

    Adicionando object.

---
+++ secao_4/dia-01/script.js
@@ -0,0 +1,12 @@
+const students39 = {
+    studentsInfo: [
+        {
+            picture:"./img/cadu.png",
+            trybe: "CH39",
+            studentName: "Cadu",
+            projectLessonsLearned: 2,
+            projectPirilampo:30,
+            projectPixelsArt: 20,
+        },
+    ]
+}
