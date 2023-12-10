6dff92ac6208c5e4c551b72432cb694e0df796ee
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2023-12-10 00:49:14 -0300

    Conceito Inicial da aula.

--- secao_4/dia-01/script.js
+++ secao_4/dia-01/script.js
@@ -1,7 +1,7 @@
 const students39 = {
     studentsInfo: [
         {
-            picture:"./img/cadu.png",
+            picture:"secao_4/dia-01/imgs/cadu.jpeg",
             trybe: "CH39",
             studentName: "Cadu",
             projectLessonsLearned: 2,
@@ -11,3 +11,21 @@ const students39 = {
     ]
 }
 
+// CAPUTRANDO OS ELEMENTOS DA PAGINA
+const elementNome = document.getElementById('name');
+const elementLessonsLearned = document.getElementById('gradeLessonsLearned');
+const elementPirilampo = document.getElementById('gradePirilampo');
+const elementPixelsArt = document.getElementById('gradePixelsArt');
+const picture = document.getElementById('picture');
+const results = document.getElementById('results');
+// Para capturar um elemento pelo id uso Byid
+
+// const titles = document.getElementsByClassName('title');
+// console.log(titles);
+// for (let title of titles){
+//     title.style.color = "green"
+// }
+
+// querySelector e um seletor que e flexivel, pode ser utilizado no contexto de um Id de uma tag ou de uma classe.
+
+// já se temos um array podemos usar o querySlectorAll. porem devo informar o que estou chamando (classe .)(id #)
