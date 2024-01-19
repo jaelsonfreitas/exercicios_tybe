9bb46e5071875c617156f0381893be307712dd4f
Jaelson Freitas <jaelsonfreitas@yahoo.com.br>
2024-01-19 07:09:02 -0300

    Aula ao vivo.

---
+++ secao_6/script2.js
@@ -0,0 +1,11 @@
+// console.log(harryPotterMovies);
+
+const addHarryPotterMovie = () => {
+    const yearElement = document.querySelector('#movie');
+    const year = yearElement.value;
+    console.log('year', year);
+};
+
+window.onload = () => {
+    const button = document.getElementById('button')
+}
